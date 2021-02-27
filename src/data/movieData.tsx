f216c543c2300b1d55b9558dcbe89efe88451ee3
Aaron Didner <adidner@gmail.com>
2021-02-27 11:49:37 -0500

    added game data and data interfaces

---
+++ src/data/movieData.tsx
@@ -0,0 +1,14 @@
+export interface movieDataInterface {
+    title: string;
+    director: string;
+    imagePath: string;
+    rating: number; //rating out of 10
+    review: string;
+    releaseDate: string;
+};
+
+export const movieData: movieDataInterface[] = [
+
+
+
+];
